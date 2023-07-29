d16473816e392b558292729cf135d0aceb835a5a
Kim Hallberg <hallberg.kim@gmail.com>
2023-07-29 03:04:13 +0200

    Add Tailwind and migrate welcome to Pages

--- resources/views/pages/todos.blade.php
+++ resources/views/pages/todos.blade.php
@@ -3,7 +3,7 @@
 use function Livewire\Volt\state;
 use App\Models\Todo;
 
-state(description: '', todos: fn () => Todo::all());
+state(description: '', todos: fn() => Todo::all());
 
 $addTodo = function () {
     Todo::create([
@@ -12,33 +12,25 @@ $addTodo = function () {
 
     $this->description = '';
     $this->todos = Todo::all();
-}
+};
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todos</title>
-</head>
-<body>
-@volt
-    <div>
-        <h1>Add Todo</h1>
-        <form wire:submit="addTodo">
-            <input type="text" wire:model="description">
-            <button type="submit">Add</button>
-        </form>
+<x-app-layout>
+    @volt
+        <div>
+            <h1>Add Todo</h1>
+            <form wire:submit="addTodo">
+                <input type="text" wire:model="description" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
+            </form>
 
-        <h2>Todos</h2>
-        <ul>
-            @foreach ($todos as $todo)
-                <li>{{ $todo->description }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endvolt
-</body>
-</html>
+            <h2>Todos</h2>
+            <ul>
+                @foreach ($todos as $todo)
+                    <li>{{ $todo->description }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endvolt
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        forms
+    ],
+};
